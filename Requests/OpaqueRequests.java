26569a3d6fdaedfd663591f61266296e7e9c4a87
jonnyschuller1 <51962101+jonnyschuller1@users.noreply.github.com>
2020-03-20 14:04:57 -0400

    Create OpaqueRequests.java

---
+++ Requests/OpaqueRequests.java
@@ -0,0 +1,38 @@
+package Requests;
+
+import java.net.*;
+import java.io.*;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class OpaqueRequests {
+        public static void main (String[] args) {
+                while(true){
+                        try {
+                                Properties prop = new Properties();
+                                String fileName = "requests.conf";
+                                InputStream configis = null;
+
+                                configis = new FileInputStream(fileName);
+                                prop.load(configis);
+
+                                String requestURL = prop.getProperty("REQUEST_URL");
+                                URL google = new URL(requestURL);
+                                InputStream is = google.openStream();
+                                is.close();
+                                String requestOutput = "Request to " + requestURL + " successful";
+                                System.out.println(requestOutput);
+                                TimeUnit.SECONDS.sleep(3);
+                        } catch (MalformedURLException ex) {
+                                System.out.println("MalformedURLException");
+                                System.exit(0);
+                        } catch (IOException ex) {
+                                System.out.println("IOException");
+                                System.exit(0);
+                        } catch (InterruptedException ex) {
+                                System.out.println("InterruptedException");
+                                System.exit(0);
+                        }
+                }
+        }
+}
