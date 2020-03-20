b462d37253c9d35f0d580d0b98ccdbee4b516578
jonnyschuller1 <51962101+jonnyschuller1@users.noreply.github.com>
2020-03-20 16:47:25 -0400

    Update OpaqueRequests.java

--- Requests/OpaqueRequests.java
+++ Requests/OpaqueRequests.java
@@ -5,6 +5,30 @@ import java.io.*;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+public class PublicOrUnmonitoredRequest{
+        public static void makeRequest(String requestUrl){
+                
+                try {
+                        URL google = new URL(requestURL);
+                        InputStream is = google.openStream();
+                        is.close();
+                        String requestOutput = "Request to " + requestURL + " successful";
+                        System.out.println(requestOutput);
+                } catch (MalformedURLException ex) {
+                        System.out.println("MalformedURLException");
+                        System.exit(0);
+                } catch (IOException ex) {
+                        System.out.println("IOException");
+                        System.exit(0);
+                } catch (InterruptedException ex) {
+                        System.out.println("InterruptedException");
+                        System.exit(0);
+                }
+
+        }
+}
+                
+
 public class OpaqueRequests {
         public static void main (String[] args) {
                 while(true){
@@ -17,11 +41,10 @@ public class OpaqueRequests {
                                 prop.load(configis);
 
                                 String requestURL = prop.getProperty("REQUEST_URL");
-                                URL google = new URL(requestURL);
-                                InputStream is = google.openStream();
-                                is.close();
-                                String requestOutput = "Request to " + requestURL + " successful";
-                                System.out.println(requestOutput);
+                                
+                                PublicOrUnmonitoredRequest pour = new PublicOrUnmonitoredRequest();
+                                pour.makeRequest(requestURL);
+
                                 TimeUnit.SECONDS.sleep(3);
                         } catch (MalformedURLException ex) {
                                 System.out.println("MalformedURLException");
